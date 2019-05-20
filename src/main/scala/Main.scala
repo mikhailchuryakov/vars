f88379ee2580df780cbcfc2a088e763b18a67227
MikhailCh <mike_ch@mail.ru>
2019-05-20 17:49:09 +0300

    add exceptions and comments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,10 +9,11 @@ trait Main {
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val file = new File("C:\\Users\\User\\Desktop\\vars\\src\\main\\resources\\file1.vars")
+    val file = new File("C:\\Users\\User\\Desktop\\vars\\src\\main\\resources\\file1.vars") // todo change path
     val reader = new Reader(file.getName)
     reader.dumpFile[IO](file).unsafeRunSync()
     println(reader.variables)
     println(reader.importList)
+    println(reader.importsMapList.head)
   }
 }
--- src/main/scala/Reader.scala
+++ src/main/scala/Reader.scala
@@ -9,6 +9,7 @@ import scala.collection.mutable.ListBuffer
 class Reader(fileName: String) {
   var variables: Map[String, String] = Map.empty
   val importList: ListBuffer[String] = new ListBuffer[String]
+  val importsMapList: ListBuffer[Map[String, String]] = new ListBuffer[Map[String, String]]
 
   private def reader[F[_]](file: File)(implicit F: Sync[F]): Resource[F, BufferedReader] =
     Resource.fromAutoCloseable(F.delay {
@@ -16,23 +17,29 @@ class Reader(fileName: String) {
     })
 
   private def dumpResource[F[_]](res: Resource[F, BufferedReader])(implicit F: Sync[F]): F[Unit] = {
-    var count = 0
+    var count = 0 // count number of line for exceptions
+
     def loop(in: BufferedReader): F[Unit] =
       F.suspend {
         val line = in.readLine()
         count += 1
         if (line != null) {
           line match {
+            // check the line for correctness
             case correctPat(name, value) => variables.get(name) match {
               case None => variables += (name -> value)
               case Some(_) => throw DuplicateVariable(fileName, count, name)
             }
+            // check the line for refer to
             case referPat(name1, name2) => variables.get(name2) match {
               case Some(value) => variables += (name1 -> value)
               case None => println(s"Vse ploho $name1 refer to $name2") // todo find in import files
             }
+            // parse imports
             case lineImportPat(_, _) => parseImports(line)
+            // new line
             case "" =>
+            // other variants
             case _ => throw IncorrectLine(fileName, count, line)
           }
 
@@ -45,15 +52,18 @@ class Reader(fileName: String) {
     res.use(loop)
   }
 
-  def dumpFile[F[_]](file: File)(implicit F: Sync[F]): F[Unit] = {
-    dumpResource(reader(file))
-  }
+  def dumpFile[F[_]](file: File)(implicit F: Sync[F]): F[Unit] = dumpResource(reader(file))
+
 
   def parseImports(line: String): Unit = {
     val imports = filePat.findAllIn(line)
-    while (imports.hasNext) {
+    while (imports.hasNext) { // remove while
       val file = imports.next()
+      val reader = new Reader(file)
+      reader.dumpFile[IO](new File(s"C:\\Users\\User\\Desktop\\vars\\src\\main\\resources\\$file.vars")).unsafeRunSync()
+      importsMapList += reader.variables
       importList += file
+
     }
   }
 }
